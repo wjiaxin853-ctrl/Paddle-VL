9a4e5f5b086692737ff0840f50c8b54ee3867b1b
wjiaxin853-ctrl <wjiaxin853@gmail.com>
2026-04-27 16:11:09 +0800

    增加DEEPSEEKOCR初始化测试脚本

--- scripts/DeepSeek-OCR-2/DeepSeek-OCR-2.py
+++ scripts/DeepSeek-OCR-2/DeepSeek-OCR-2.py
@@ -1,8 +1,6 @@
 from transformers import AutoModel, AutoTokenizer
 import torch
 import os
-import types
-
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
 model_name = 'deepseek-ai/DeepSeek-OCR-2'
 
@@ -12,8 +10,8 @@ model = model.eval().cuda().to(torch.bfloat16)
 
 # prompt = "<image>\nFree OCR. "
 prompt = "<image>\n<|grounding|>Convert the document to markdown. "
-image_file = '/demo/images/手持身份证.jpg'
-output_path = '/Users/wjx/Documents/wjx/Paddle-VL/output/DeepSeek-OCR-2'
+image_file = 'your_image.jpg'
+output_path = 'your/output/dir'
 
 
 res = model.infer(tokenizer, prompt=prompt, image_file=image_file, output_path = output_path, base_size = 1024, image_size = 768, crop_mode=True, save_results = True)
